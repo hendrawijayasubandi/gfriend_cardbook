f0f48ddc3c5b027c182ba4e3c70435319c2b2f9b
Hendra Wijaya Subandi <hendrawijayasubandi@gmail.com>
2023-02-21 21:42:09 +0700

    mod file main_screen.dart

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,55 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:gfriend_cardbook/model/all_collection.dart';
-import 'package:gfriend_cardbook/detail_screen.dart';
 
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('GFRIEND CARDBOOK'),
-      ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          final AllCollection collection = AllCollectionList[index];
-          return InkWell(
-            onTap: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return DetailScreen();
-              }));
-            },
-            child: Card(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Expanded(
-                    flex: 1,
-                    child: Image.asset(collection.imageAlbum),
-                  ),
-                  Expanded(
-                    flex: 2,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            collection.name,
-                            style: const TextStyle(fontSize: 16.0),
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                        ],
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-          );
-        },
-        itemCount: AllCollection.length,
+        title: const Text('Wisata Bandung'),
       ),
     );
   }
