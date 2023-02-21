acc0b236c2ab5b63d2b75ba7845fae5a91d545a7
Hendra Wijaya Subandi <hendrawijayasubandi@gmail.com>
2023-02-21 15:27:29 +0700

    mod file all_collection.dart

--- lib/all_collection.dart
+++ lib/all_collection.dart
@@ -20,11 +20,162 @@ class AllCollection {
 
 var AllCollectionList = [
   AllCollection(
-    name: '',
+    name: 'Season of Glass',
     imageEmblem: '',
     imageLobbyBG: '',
-    date: '',
-    description: '',
+    date: 'January 15, 2015',
+    description:
+        '1. Intro (Season of Glass)\n2. Glass Bead (Title)\n3. Neverland\n4. White\n5. Glass Bead (Inst.)',
+    imageAlbum: '',
+    imageCards: ['', '', ''],
+  ),
+  AllCollection(
+    name: 'Flower Bud',
+    imageEmblem: '',
+    imageLobbyBG: '',
+    date: 'July 23, 2015',
+    description:
+        '1. Intro (Flower Bud)\n2. Me Gustas Tú (Title)\n3. Under the Sky\n4. One\n5. My Buddy\n6. Me Gustas Tú (Inst.)',
+    imageAlbum: '',
+    imageCards: ['', '', ''],
+  ),
+  AllCollection(
+    name: 'Snowflake',
+    imageEmblem: '',
+    imageLobbyBG: '',
+    date: 'January 25, 2016',
+    description:
+        '1. Intro (Snowflake)\n2. Rough (Title)\n3. Say My Name\n4. Luv Star\n5. Someday\n6. Trust\n7. Rough (Inst.)',
+    imageAlbum: '',
+    imageCards: ['', '', ''],
+  ),
+  AllCollection(
+    name: 'LOL',
+    imageEmblem: '',
+    imageLobbyBG: '',
+    date: 'July 11, 2016',
+    description:
+        '1. Intro\n2. Fall In Love\n3. Navillera (Title)\n4. LOL\n5. Distance\n6. Water Flower\n7. Mermaid\n8. Sunshine\n9. Compas\n10. Click\n11. Gone With The Wind\n12. Navillera (Inst.)',
+    imageAlbum: '',
+    imageCards: ['', '', ''],
+  ),
+  AllCollection(
+    name: 'The Awakening',
+    imageEmblem: '',
+    imageLobbyBG: '',
+    date: 'March 6, 2017',
+    description:
+        '1. Hear The Wind Song\n2. Fingertip (Title)\n3. Contrail\n4. Please Save My Earth\n5. Rain In The Springtime\n6. Crush',
+    imageAlbum: '',
+    imageCards: ['', '', ''],
+  ),
+  AllCollection(
+    name: 'Parallel',
+    imageEmblem: '',
+    imageLobbyBG: '',
+    date: 'August 1, 2017',
+    description:
+        '1. Intro (Belifef)\n2. Love Whisper (Title)\n3. Ave Maria\n4. One-half\n5. Life is a Party\n6. Red Umbrella\n7. Falling Asleep Again\n8. Love Whisper (Inst.)',
+    imageAlbum: '',
+    imageCards: ['', '', ''],
+  ),
+  AllCollection(
+    name: 'Rainbow',
+    imageEmblem: '',
+    imageLobbyBG: '',
+    date: 'Spetember 13, 2017',
+    description:
+        '1. Intro (Belifef)\n2. Summer Rain (Title)\n3. Rainbow\n4. Love Whisper\n5. Ave Maria\n6. One-half\n7. Life is a Party\n8. Red Umbrella\n9. Falling Asleep Again\n10. Summer Rain (Inst.)',
+    imageAlbum: '',
+    imageCards: ['', '', ''],
+  ),
+  AllCollection(
+    name: 'Time for the Moon Night',
+    imageEmblem: '',
+    imageLobbyBG: '',
+    date: 'April 30, 2018',
+    description:
+        '1. Intro (Daytime)\n2. Time for the Moon Night (Title)\n3. Love Bug\n4. Flower Garden\n5. Tik Tik\n6. Bye\n7. You Are My Star\n8. Time for The Moon Night (Inst.)',
+    imageAlbum: '',
+    imageCards: ['', '', ''],
+  ),
+  AllCollection(
+    name: 'Sunny Summer',
+    imageEmblem: '',
+    imageLobbyBG: '',
+    date:
+        '1. Sunny Summer (Title)\n2. Vacation\n3. Sweety\n4. Windy Windy\n5. Love In The Air',
+    description:
+        '1. Intro (Season of Glass)\n2. Glass Bead (Title)\n3. Neverland\n4. White\n5. Glass Bead (Inst.)',
+    imageAlbum: '',
+    imageCards: ['', '', ''],
+  ),
+  AllCollection(
+    name: 'Time for Us',
+    imageEmblem: '',
+    imageLobbyBG: '',
+    date: 'January 14, 2019',
+    description:
+        '1. Sunrise (Title)\n2. You Are Not Alone\n3. L.U.V\n4. Glow\n5. Our Secret\n6. Only 1\n7. Truly Love\n8. Show Up\n9. It’s You\n10. A Starry Sky\n11. Love Oh Love\n12. Memoria (Korean ver.)\n13. Sunrise (Inst.)',
+    imageAlbum: '',
+    imageCards: ['', '', ''],
+  ),
+  AllCollection(
+    name: 'Go Go GFRIEND!',
+    imageEmblem: '',
+    imageLobbyBG: '',
+    date: 'Mei 18, 2019',
+    description: '1. Monday Blues\n2. Shy Boy',
+    imageAlbum: '',
+    imageCards: ['', '', ''],
+  ),
+  AllCollection(
+    name: 'FEVER SEASON',
+    imageEmblem: '',
+    imageLobbyBG: '',
+    date: 'July 1, 2019',
+    description:
+        '1. Fever (Title)\n2. Mr. Blue\n3. Smile\n4. Wish\n5. Paradise\n6. Hope\n7. Flower (Korean ver.)\n8. Fever (Inst.)',
+    imageAlbum: '',
+    imageCards: ['', '', ''],
+  ),
+  AllCollection(
+    name: '回: Labyrinth',
+    imageEmblem: '',
+    imageLobbyBG: '',
+    date: 'February 3, 2020',
+    description:
+        '1. Labyrinth\n2. Crossroads (Title)\n3. Here We Are\n4. Eclispe\n5. Dreamcatcher\n6. From Me',
+    imageAlbum: '',
+    imageCards: ['', '', ''],
+  ),
+  AllCollection(
+    name: '回: Song of the Sirens',
+    imageEmblem: '',
+    imageLobbyBG: '',
+    date: 'July 13, 2020',
+    description:
+        '1. Apple (Title)\n2. Eye of the Storm\n3. Room of Mirrors\n4. Tarot Cards\n5. Crème Brûlée\n6. Stairs in the North',
+    imageAlbum: '',
+    imageCards: ['', '', ''],
+  ),
+  AllCollection(
+    name: '回:Walpurgis Night',
+    imageEmblem: '',
+    imageLobbyBG: '',
+    date: '9 November, 2020',
+    description:
+        '1. Mago (Title)\n2. Love Spell\n3. Three of Cups\n4. GRWM\n5. Secret Diary (Yerin & SinB)\n6. Better Me (Sowon & Umji)\n7. Nigth Drive (Eunha & Yuju)\n8. Apple\n9. Crossroads\n10. Labyrinth\n11. Wheel of the Year',
+    imageAlbum: '',
+    imageCards: ['', '', ''],
+  ),
+  AllCollection(
+    name: 'Season of Glass',
+    imageEmblem: '',
+    imageLobbyBG: '',
+    date: 'January 15, 2015',
+    description:
+        '1. Intro (Season of Glass)\n2. Glass Bead (Title)\n3. Neverland\n4. White\n5. Glass Bead (Inst.)',
     imageAlbum: '',
     imageCards: ['', '', ''],
   ),
