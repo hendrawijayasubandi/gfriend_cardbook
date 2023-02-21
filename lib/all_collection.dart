0a1ec1339e36e7a9fef0bb71639a3e388d8fc89e
Hendra Wijaya Subandi <hendrawijayasubandi@gmail.com>
2023-02-21 16:38:39 +0700

    update file

--- lib/all_collection.dart
+++ lib/all_collection.dart
@@ -54,7 +54,7 @@ var AllCollectionList = [
     imageLobbyBG: 'images/lobbybg/1_l.i.png',
     date: 'July 23, 2015',
     description:
-        '1. Intro (Flower Bud)\n2. Me Gustas Tú (Title)\n3. Under the Sky\n4. One\n5. My Buddy\n6. Me Gustas Tú (Inst.)',
+        'Tracklist:\n1. Intro (Flower Bud)\n2. Me Gustas Tú (Title)\n3. Under the Sky\n4. One\n5. My Buddy\n6. Me Gustas Tú (Inst.)',
     imageAlbum: '',
     imageCards: ['', '', ''],
   ),
@@ -64,7 +64,7 @@ var AllCollectionList = [
     imageLobbyBG: 'images/lobbybg/2_l.i.png',
     date: 'January 25, 2016',
     description:
-        '1. Intro (Snowflake)\n2. Rough (Title)\n3. Say My Name\n4. Luv Star\n5. Someday\n6. Trust\n7. Rough (Inst.)',
+        'Tracklist:\n1. Intro (Snowflake)\n2. Rough (Title)\n3. Say My Name\n4. Luv Star\n5. Someday\n6. Trust\n7. Rough (Inst.)',
     imageAlbum: '',
     imageCards: ['', '', ''],
   ),
@@ -74,7 +74,7 @@ var AllCollectionList = [
     imageLobbyBG: 'images/lobbybg/3_l.i.png',
     date: 'July 11, 2016',
     description:
-        '1. Intro\n2. Fall In Love\n3. Navillera (Title)\n4. LOL\n5. Distance\n6. Water Flower\n7. Mermaid\n8. Sunshine\n9. Compas\n10. Click\n11. Gone With The Wind\n12. Navillera (Inst.)',
+        'Tracklist:\n1. Intro\n2. Fall In Love\n3. Navillera (Title)\n4. LOL\n5. Distance\n6. Water Flower\n7. Mermaid\n8. Sunshine\n9. Compas\n10. Click\n11. Gone With The Wind\n12. Navillera (Inst.)',
     imageAlbum: '',
     imageCards: ['', '', ''],
   ),
@@ -84,7 +84,7 @@ var AllCollectionList = [
     imageLobbyBG: 'images/lobbybg/4_l.i.png',
     date: 'March 6, 2017',
     description:
-        '1. Hear The Wind Song\n2. Fingertip (Title)\n3. Contrail\n4. Please Save My Earth\n5. Rain In The Springtime\n6. Crush',
+        'Tracklist:\n1. Hear The Wind Song\n2. Fingertip (Title)\n3. Contrail\n4. Please Save My Earth\n5. Rain In The Springtime\n6. Crush',
     imageAlbum: '',
     imageCards: ['', '', ''],
   ),
@@ -94,7 +94,7 @@ var AllCollectionList = [
     imageLobbyBG: 'images/lobbybg/5_l.i.png',
     date: 'August 1, 2017',
     description:
-        '1. Intro (Belifef)\n2. Love Whisper (Title)\n3. Ave Maria\n4. One-half\n5. Life is a Party\n6. Red Umbrella\n7. Falling Asleep Again\n8. Love Whisper (Inst.)',
+        'Tracklist:\n1. Intro (Belifef)\n2. Love Whisper (Title)\n3. Ave Maria\n4. One-half\n5. Life is a Party\n6. Red Umbrella\n7. Falling Asleep Again\n8. Love Whisper (Inst.)',
     imageAlbum: '',
     imageCards: ['', '', ''],
   ),
@@ -104,7 +104,7 @@ var AllCollectionList = [
     imageLobbyBG: 'images/lobbybg/6_l.i.png',
     date: 'Spetember 13, 2017',
     description:
-        '1. Intro (Belifef)\n2. Summer Rain (Title)\n3. Rainbow\n4. Love Whisper\n5. Ave Maria\n6. One-half\n7. Life is a Party\n8. Red Umbrella\n9. Falling Asleep Again\n10. Summer Rain (Inst.)',
+        'Tracklist:\n1. Intro (Belifef)\n2. Summer Rain (Title)\n3. Rainbow\n4. Love Whisper\n5. Ave Maria\n6. One-half\n7. Life is a Party\n8. Red Umbrella\n9. Falling Asleep Again\n10. Summer Rain (Inst.)',
     imageAlbum: '',
     imageCards: ['', '', ''],
   ),
@@ -114,7 +114,7 @@ var AllCollectionList = [
     imageLobbyBG: 'images/lobbybg/7_l.i.png',
     date: 'April 30, 2018',
     description:
-        '1. Intro (Daytime)\n2. Time for the Moon Night (Title)\n3. Love Bug\n4. Flower Garden\n5. Tik Tik\n6. Bye\n7. You Are My Star\n8. Time for The Moon Night (Inst.)',
+        'Tracklist:\n1. Intro (Daytime)\n2. Time for the Moon Night (Title)\n3. Love Bug\n4. Flower Garden\n5. Tik Tik\n6. Bye\n7. You Are My Star\n8. Time for The Moon Night (Inst.)',
     imageAlbum: '',
     imageCards: ['', '', ''],
   ),
@@ -124,7 +124,7 @@ var AllCollectionList = [
     imageLobbyBG: 'images/lobbybg/8_l.i.png',
     date: '',
     description:
-        '1. Sunny Summer (Title)\n2. Vacation\n3. Sweety\n4. Windy Windy\n5. Love In The Air',
+        'Tracklist:\n1. Sunny Summer (Title)\n2. Vacation\n3. Sweety\n4. Windy Windy\n5. Love In The Air',
     imageAlbum: '',
     imageCards: ['', '', ''],
   ),
@@ -134,7 +134,7 @@ var AllCollectionList = [
     imageLobbyBG: 'images/lobbybg/9_l.i.png',
     date: 'January 14, 2019',
     description:
-        '1. Sunrise (Title)\n2. You Are Not Alone\n3. L.U.V\n4. Glow\n5. Our Secret\n6. Only 1\n7. Truly Love\n8. Show Up\n9. It’s You\n10. A Starry Sky\n11. Love Oh Love\n12. Memoria (Korean ver.)\n13. Sunrise (Inst.)',
+        'Tracklist:\n1. Sunrise (Title)\n2. You Are Not Alone\n3. L.U.V\n4. Glow\n5. Our Secret\n6. Only 1\n7. Truly Love\n8. Show Up\n9. It’s You\n10. A Starry Sky\n11. Love Oh Love\n12. Memoria (Korean ver.)\n13. Sunrise (Inst.)',
     imageAlbum: '',
     imageCards: ['', '', ''],
   ),
@@ -143,7 +143,7 @@ var AllCollectionList = [
     imageEmblem: '',
     imageLobbyBG: 'images/lobbybg/16_l.i.png',
     date: 'Mei 18, 2019',
-    description: '1. Monday Blues\n2. Shy Boy',
+    description: 'Tracklist:\n1. Monday Blues\n2. Shy Boy',
     imageAlbum: '',
     imageCards: ['', '', ''],
   ),
@@ -153,7 +153,7 @@ var AllCollectionList = [
     imageLobbyBG: 'images/lobbybg/10_l.i.png',
     date: 'July 1, 2019',
     description:
-        '1. Fever (Title)\n2. Mr. Blue\n3. Smile\n4. Wish\n5. Paradise\n6. Hope\n7. Flower (Korean ver.)\n8. Fever (Inst.)',
+        'Tracklist:\n1. Fever (Title)\n2. Mr. Blue\n3. Smile\n4. Wish\n5. Paradise\n6. Hope\n7. Flower (Korean ver.)\n8. Fever (Inst.)',
     imageAlbum: '',
     imageCards: ['', '', ''],
   ),
@@ -163,7 +163,7 @@ var AllCollectionList = [
     imageLobbyBG: 'images/lobbybg/11_l.i.png',
     date: 'February 3, 2020',
     description:
-        '1. Labyrinth\n2. Crossroads (Title)\n3. Here We Are\n4. Eclispe\n5. Dreamcatcher\n6. From Me',
+        'Tracklist:\n1. Labyrinth\n2. Crossroads (Title)\n3. Here We Are\n4. Eclispe\n5. Dreamcatcher\n6. From Me',
     imageAlbum: '',
     imageCards: ['', '', ''],
   ),
@@ -173,7 +173,7 @@ var AllCollectionList = [
     imageLobbyBG: 'images/lobbybg/14_l.i.png',
     date: 'July 13, 2020',
     description:
-        '1. Apple (Title)\n2. Eye of the Storm\n3. Room of Mirrors\n4. Tarot Cards\n5. Crème Brûlée\n6. Stairs in the North',
+        'Tracklist:\n1. Apple (Title)\n2. Eye of the Storm\n3. Room of Mirrors\n4. Tarot Cards\n5. Crème Brûlée\n6. Stairs in the North',
     imageAlbum: '',
     imageCards: ['', '', ''],
   ),
@@ -183,17 +183,7 @@ var AllCollectionList = [
     imageLobbyBG: 'images/lobbybg/36_l.i.png',
     date: '9 November, 2020',
     description:
-        '1. Mago (Title)\n2. Love Spell\n3. Three of Cups\n4. GRWM\n5. Secret Diary (Yerin & SinB)\n6. Better Me (Sowon & Umji)\n7. Nigth Drive (Eunha & Yuju)\n8. Apple\n9. Crossroads\n10. Labyrinth\n11. Wheel of the Year',
-    imageAlbum: '',
-    imageCards: ['', '', ''],
-  ),
-  AllCollection(
-    name: 'Season of Glass',
-    imageEmblem: '',
-    imageLobbyBG: '',
-    date: 'January 15, 2015',
-    description:
-        '1. Intro (Season of Glass)\n2. Glass Bead (Title)\n3. Neverland\n4. White\n5. Glass Bead (Inst.)',
+        'Tracklist:\n1. Mago (Title)\n2. Love Spell\n3. Three of Cups\n4. GRWM\n5. Secret Diary (Yerin & SinB)\n6. Better Me (Sowon & Umji)\n7. Nigth Drive (Eunha & Yuju)\n8. Apple\n9. Crossroads\n10. Labyrinth\n11. Wheel of the Year',
     imageAlbum: '',
     imageCards: ['', '', ''],
   ),
